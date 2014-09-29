54dc1c7a875ed1857617a888ef92119baf201a27
lijiajia <lijiaji_1010@126.com>
2014-09-29 13:58:57 +0800

    merge

---
+++ test/test.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Mon 29 Sep 2014 01:56:03 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"hello world!"<<endl;
+}
